a928d6fd1688b47c854a0bb149162c436edee6b4
SteveYang777 <steve79846@gmail.com>
2020-09-08 23:11:44 +0800

    20200908 First update

---
+++ 附檔/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxTransmit = new System.Windows.Forms.TextBox();
+            this.textBoxReceive = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // textBoxTransmit
+            // 
+            this.textBoxTransmit.Location = new System.Drawing.Point(123, 26);
+            this.textBoxTransmit.Name = "textBoxTransmit";
+            this.textBoxTransmit.Size = new System.Drawing.Size(111, 22);
+            this.textBoxTransmit.TabIndex = 0;
+            this.textBoxTransmit.TextChanged += new System.EventHandler(this.textBoxTransmit_TextChanged);
+            // 
+            // textBoxReceive
+            // 
+            this.textBoxReceive.Location = new System.Drawing.Point(50, 60);
+            this.textBoxReceive.Multiline = true;
+            this.textBoxReceive.Name = "textBoxReceive";
+            this.textBoxReceive.Size = new System.Drawing.Size(264, 270);
+            this.textBoxReceive.TabIndex = 5;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.Handshake = System.IO.Ports.Handshake.RequestToSend;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 150;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.radioButton1.ForeColor = System.Drawing.Color.Coral;
+            this.radioButton1.Location = new System.Drawing.Point(50, 28);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 20);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "加值";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("新細明體", 12F);
+            this.radioButton2.ForeColor = System.Drawing.Color.Coral;
+            this.radioButton2.Location = new System.Drawing.Point(256, 28);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(58, 20);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.Text = "扣款";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 366);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBoxReceive);
+            this.Controls.Add(this.textBoxTransmit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxTransmit;
+        private System.Windows.Forms.TextBox textBoxReceive;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ 附檔/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        String IDcard="";
+        String ID = "";
+        bool add = true;
+        bool sub = false;
+        public Form1()
+        {
+            InitializeComponent();
+            //serialPort1.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            timer1.Start();
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private byte[] hexStringToByte(string hexString)
+        {
+
+            byte[] returnedBytes = new byte[hexString.Length / 2];
+            for (int i = 0; i < returnedBytes.Length; i++)
+                returnedBytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+            return returnedBytes;
+
+        }
+
+        //將數值位元組轉成16進位字串
+
+        private String byteToHexString(byte[] buffer)
+
+        {
+
+            String hexString = "";
+
+            for (int i = 0; i < buffer.Length; i++)
+                hexString += buffer[i].ToString("X2"); //將資料先轉16進位
+
+            return hexString;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            try
+            {
+                if (!serialPort1.IsOpen)
+                    serialPort1.Open();
+
+                byte[] bufferSend;
+                bufferSend = hexStringToByte("55");
+                serialPort1.Write(bufferSend, 0, bufferSend.Length);
+               
+                //等待接收
+                System.Threading.Thread.Sleep(150);
+                byte[] bufferReceive = new byte[serialPort1.BytesToRead];
+                serialPort1.Read(bufferReceive, 0, bufferReceive.Length);
+                
+                ID = byteToHexString(bufferReceive);
+                String check = byteToHexString(bufferReceive).Substring(0,2);
+                //this.textBoxReceive.Text = "ID = "+ ID + "IDcard = " + IDcard + "check = " + check +"\r\n" + this.textBoxReceive.Text;
+                if (check == "86")
+                {
+                    if (ID != IDcard)
+                    {
+                        IDcard = ID;
+                        //輸入指令
+                        bufferSend = hexStringToByte("520400");
+                        serialPort1.Write(bufferSend, 0, bufferSend.Length);
+
+                        //等待接收
+                        System.Threading.Thread.Sleep(150);
+                        bufferReceive = new byte[serialPort1.BytesToRead];
+                        serialPort1.Read(bufferReceive, 0, bufferReceive.Length);
+
+                        //16轉換10
+                        String receive = byteToHexString(bufferReceive).Substring(2, 4);
+                        int r = Convert.ToInt32(receive, 16);
+
+                        int num = 0;
+                        String s = textBoxTransmit.Text.ToString();
+                        //金額框轉換
+                        if (!String.IsNullOrEmpty(s))
+                        {
+                            num = int.Parse(s);
+                        }
+
+                        int f = 0;
+                        //計算
+                        if (add == true && r + num < 10000)
+                        {
+                            r += num;
+                        }
+                        else if (sub == true && r > num)
+                        {
+                            r -= num;
+                        }
+                        else if(add == true && r + num > 10000)
+                        {
+                            f = 1;
+                        }
+                        else if(sub == true && r < num)
+                        {
+                            f = 2;
+                        }
+
+                        receive = r.ToString("X4");
+                        /*receive = Convert.ToString(r, 16);
+                        int length = 4 - receive.Length;
+                        while (receive.Length < 4)
+                        {
+                            receive = "0" + receive;
+                        }*/
+
+                        //輸入指令
+                        s = "570400" + receive + "0000000000000000000000000000";
+
+                        bufferSend = hexStringToByte(s);
+                        serialPort1.Write(bufferSend, 0, bufferSend.Length);
+                        System.Threading.Thread.Sleep(150);
+                        bufferReceive = new byte[serialPort1.BytesToRead];
+                        serialPort1.Read(bufferReceive, 0, bufferReceive.Length);
+                        if (f == 1)
+                            this.textBoxReceive.Text = ">加值金額已達上限,餘額: " + r + "元\r\n" + this.textBoxReceive.Text;
+                        else if (f == 2)
+                            this.textBoxReceive.Text = ">餘額不足,餘額: " + r + "元\r\n" + this.textBoxReceive.Text;
+                        else if (add == true)
+                            this.textBoxReceive.Text = ">加值" + num + "元，餘額:" + r + "元\r\n" + this.textBoxReceive.Text;
+                        else if (sub == true)
+                            this.textBoxReceive.Text = ">扣款" + num + "元，餘額:" + r + "元\r\n" + this.textBoxReceive.Text;
+                        System.Threading.Thread.Sleep(100);
+                    }
+                }
+                else
+                    IDcard = "";
+            }
+            catch
+            {
+            }
+
+            timer1.Start();
+        }
+
+        private void textBoxTransmit_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            add = true;
+            sub = false;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            sub = true;
+            add = false;
+        }
+    }
+}
